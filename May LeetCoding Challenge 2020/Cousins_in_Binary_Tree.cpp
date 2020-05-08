2c4a297c2218a50cfcd533dec52398b7269d17e7
Kaushal Sahu <kaushal.raj.sahu@gmail.com>
2020-05-08 11:31:26 +0530

    Best Implementation Found

--- May LeetCoding Challenge 2020/Cousins_in_Binary_Tree.cpp
+++ May LeetCoding Challenge 2020/Cousins_in_Binary_Tree.cpp
@@ -11,39 +11,25 @@
  */
 class Solution {
 public:
+    TreeNode* xParent = NULL, *yParent = NULL;
+    int xDepth = -1, yDepth = -2;
     bool isCousins(TreeNode* root, int x, int y) {
-        if (getDepth(root, x, 1) == getDepth(root, y, 1) && !isSibling(root, x, y)) {
-            return true;
-        }
-        return false;
+        dfs(root, NULL, x, y, 0);
+        return xDepth == yDepth && xParent != yParent;
     }
-    int getDepth(TreeNode* ptr, int val, int depth) {
-        if (ptr == NULL) {
-            return 0;
+    void dfs(TreeNode* root, TreeNode* parent, int x, int y, int depth) {
+        if (root == NULL) {
+            return;
         }
-        if (ptr->val == val) {
-            return depth;
+        if (x == root->val) {
+            xParent = parent;
+            xDepth = depth;
+        } else if (y == root->val) {
+            yParent = parent;
+            yDepth = depth;
+        } else {
+            dfs(root->left, root, x, y, depth + 1);
+            dfs(root->right, root, x, y, depth + 1);
         }
-        int downLevel = getDepth(ptr->left, val, depth + 1);
-        if (downLevel != 0) {
-            return downLevel;
-        }
-        downLevel = getDepth(ptr->right, val, depth + 1);
-        return downLevel;
-    }
-    bool isSibling(TreeNode* ptr, int x, int y) {
-        if (ptr == NULL) {
-            return false;
-        }
-        if (ptr->left && ptr->right) {
-            if ((ptr->left->val == x && ptr->right->val == y) 
-                || (ptr->left->val == y && ptr->right->val == x)) {
-                return true;
-            }
-        }
-        isSibling(ptr->left, x, y);
-        isSibling(ptr->right, x, y);
-        return false;
-
     }
 };
