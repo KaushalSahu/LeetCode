40afe99d5039919e77624c755ff879fb48e06b63
kaushalsahu <kaushal.raj.sahu@gmail.com>
2020-05-27 12:21:30 +0530

    Continous Array

---
+++ May LeetCoding Challenge 2020/Continous_Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        if (nums.size() <= 1)
+            return 0;
+        
+        unordered_map<int, int> cache;
+        int count = nums[0] == 1 ? 1 : -1;
+        
+        cache[0] = -1;
+        cache[count] = 0;
+        int maximum = 0;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            count = (nums[i] == 1) ? count + 1 : count - 1;
+            
+            if (cache.find(count) != cache.end())
+                maximum = max(maximum, i - cache[count]);
+            else
+                cache[count] = i;
+            
+        }
+        return maximum;
+    } 
+};
